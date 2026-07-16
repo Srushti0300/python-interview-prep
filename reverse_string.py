025794140788c73e896a455541d2c9aa8368314d
Srushti_Jamdar40 <srustijamdar40@gmail.com>
2026-07-16 21:51:44 +0530

    Add: reverse string problem

---
+++ reverse_string.py
@@ -0,0 +1,10 @@
+# Problem: Reverse a string without using slicing
+# Example: Input "hello" -> Output "olleh"
+
+def reverse_string(s):
+    reversed_str = ""              # empty string to build result
+    for char in s:                 # loop through each character in s
+        reversed_str = char + reversed_str   # add new char to the FRONT
+    return reversed_str
+
+print(reverse_string("hello"))
