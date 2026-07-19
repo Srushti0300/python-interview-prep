f0c7d1a4ad32b57abde4464810bc4d6701f40751
Srushti Jamdar <srushtijamdar40@gmail.com>
2026-07-19 17:36:23 +0530

    Clean up print.py after merge

--- print.py
+++ print.py
@@ -1,7 +1,3 @@
 print("Hello World")
-<<<<<<< HEAD
-print("My name is Srushti")
-print("Learning GitHub with ChatGPT!")
-=======
 print("My name is Srushti Jamdar")
->>>>>>> a785d952bba3c85c8bee9e5694ce73be88cf1d0b
+print("Learning GitHub with ChatGPT!")
