af776c4ec5248ced603803639c8b8aa02c4d3bcb
Srushti Jamdar <navnath.srushti24@sanjivani.edu.in>
2026-07-25 22:50:05 +0530

    Add append and sort examples to List.py

--- List.py
+++ List.py
@@ -1,8 +1,16 @@
-marks = [12,13,14,15,25,23]
-print(marks)
-print(type(marks))
-print(len(marks))
-print(marks[0])
+list = [12,13,14,15,25,23]
+print(list)
+print(type(list))
+print(len(list))
+print(list[0])
 
 #list slicing
-print(marks[1:4])
+print(list[1:4])
+
+#append
+list.append(90)  #adds one element at the end
+print(list)
+
+#sort
+list.sort()  #sorts in ascending order
+print(list) 
