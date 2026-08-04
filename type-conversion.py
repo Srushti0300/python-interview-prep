0fdd5ed971ff10a3cebf569b8d3c05e3ac97daed
Srushti Jamdar <navnath.srushti24@sanjivani.edu.in>
2026-08-04 21:57:01 +0530

    Updated type-conversion

--- type-conversion.py
+++ type-conversion.py
@@ -1,6 +1,6 @@
 age = int(input("Enter your age: "))
 height = float(input("Enter your height: "))
-name = input("Enter your name: ")
+name = input("Enter your weight: ")
 
 print(type(age))
 print(type(height))
