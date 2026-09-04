9d3a33e4014cd0b9c7283ed526b07c1cbf5911b6
Srushti_Jamdar40 <navnath.srushti24@sanjivani.edu.in>
2026-09-04 21:44:54 +0530

    Update print.py

--- print.py
+++ print.py
@@ -1,4 +1,4 @@
 print("Hello World")
-print("My name is Srushti Jamdar")
-print("Learning GitHub with ChatGPT!")
+print("My name is Srushti Navnath Jamdar")
+print("Learning GitHub with ChatGPT and Claude!")
 print("I am a B-Tech Student")
